47592e0b19c7b132373d91689f5d8ac3a596e6fa
shinbeomsik <sbsdkssud@gmail.com>
2023-09-04 21:25:07 +0900

    Revert "ApplicationContext 를 스프링 컨테이너"
    This reverts commit 1e1fa66334ed9f3cf1f96bd05399bf31cf325a7c.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,8 +1,5 @@
 package hello.core;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -13,33 +10,29 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
-@Configuration
 public class AppConfig {
-
-	@Bean
+	
 	public MemberService memberService() {
-		return new MemberServiceImpl(memberRepository()); // 생성자주입
+		return new MemberServiceImpl(memberRepository()); //생성자주입
 	}
 
-	@Bean
-	public MemberRepository memberRepository() {
+	private MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
-
 	// └ 이거 리턴타입은 구체클래스가 아니라 인터페이스를 선택해야됨
-	@Bean
+	
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
-
-	@Bean
+	
 	public DiscountPolicy discountPolicy() {
-		// return new FixDiscountPolicy(); //여기만 바꾸면 정해진 금액의 할인 아니면 비율로 할인 이렇게 설정가능
-		return new RateDiscountPolicy(); // 이렇게 되면 구성 역활을 담당하는 AppConfig만 바꾸면된다
-	} // 사용 역활을 담당하는 어떤 코드들도 번경할 필요없다.
+		//return new FixDiscountPolicy();  //여기만 바꾸면 정해진 금액의 할인 아니면 비율로 할인 이렇게 설정가능
+		return new RateDiscountPolicy();   //이렇게 되면 구성 역활을 담당하는  AppConfig만 바꾸면된다
+	}                                      // 사용 역활을 담당하는 어떤 코드들도 번경할 필요없다. 
 }
 
 // 이렇게 되면 에플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할수 있다.
+
 
 //AppConfig는 에플리케이션의 실제 동작에 필요한 '구현 객체를 생성'
 //AppConfig는 생성한 객체 인스턴스의 참조를  생성자를 통해서 주입(연결)
--- src/main/java/hello/core/OderApp.java
+++ src/main/java/hello/core/OderApp.java
@@ -1,8 +1,5 @@
 package hello.core;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -15,13 +12,10 @@ public class OderApp {
 
 	public static void main(String[] args) {
 		
-		//AppConfig appConfig = new AppConfig();
-		//MemberService memberService = appConfig.memberService();
-		//OrderService orderService = appConfig.orderService();
-		
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-		MemberService memberService = applicationContext.getBean("memberService" , MemberService.class); 
-		OrderService orderService = applicationContext.getBean("orderService",OrderService.class);
+		AppConfig appConfig = new AppConfig();
+				
+		MemberService memberService = appConfig.memberService();
+		OrderService orderService = appConfig.orderService();
 		
 		Long memberId = 1L;
 		Member member = new Member(memberId, "memberA", Grade.VIP);
