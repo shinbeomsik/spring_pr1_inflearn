31b0182690c064bb28aa51ec122e20362fa37350
shinbeomsik <sbsdkssud@gmail.com>
2023-09-07 18:33:03 +0900

    싱글톤 컨테이너
    스프링 컨테이너 덕분에 요청이 올 때 마다 객체를 생성하는것이 아닌 이미 만들어진 객체를 공유해서 효율적으로 사용 가능

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -3,6 +3,8 @@ package hello.core.singleton;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
@@ -50,5 +52,28 @@ public class SingletonTest {
 		Assertions.assertThat(singletonService1).isSameAs(singletonService2);
 		// same ==
 		// equal 
+	}
+	
+	@Test
+	@DisplayName("스프링 컨테이너와 싱글톤")
+	void springContainer() {
+		//AppConfig appConfig = new AppConfig();
+		
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		// 1. 조회 : 호풀 할 떄 마다 객체를 생성
+		MemberService memberService1 = ac.getBean("memberService",MemberService.class);
+		
+		// 2. 조회 : 호풀 할 떄 마다 객체를 생성
+		MemberService memberService2 =  ac.getBean("memberService",MemberService.class);
+		
+
+		// 참조값이 다른것을 확인
+		System.out.println("memberService1 = " + memberService1);
+		System.out.println("memberService2 = " + memberService2);
+
+		// memberService1 =/= memberService2
+		Assertions.assertThat(memberService1).isSameAs(memberService2);
+		
 	}
 }
