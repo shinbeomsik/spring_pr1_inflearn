31f589ba1c2909382fc2bf0838c9a7ced51a04c8
shinbeomsik <sbsdkssud@gmail.com>
2023-09-07 17:30:41 +0900

    싱클톤 패턴
    *** 싱글톤 패턴의 문제점
    1.싱클톤 패턴을 구현하는 코드가 많이 들어간다
    2.의존 관계상 클라이언트가 구체클래스에 의존 --> DIP 를 위반
    3.클라이언트가 구체 클래스에 의존해서 OCP 원칙을 위반할 가능성이 높다
    4.테스트하기 어렵다.
    5.내부 속성을 변경하거나 초기화하기 어렵다.
    6.private 생성자로 자식 클래시를 만들기 어렵다.
    7.유연성이 떨어진다.
    
    이러한 문제점이 있지만
    스프링 프레임워크에서 이러한 싱글톤 패턴의 문제점을 해결 (단점은 제거하면서 싱글톤으로 관리)

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -8,26 +8,47 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 
 public class SingletonTest {
-	
-	
+
 	@Test
 	@DisplayName("스프링 없는 순수한 DI 컨테이너")
 	void pureContainer() {
 		AppConfig appConfig = new AppConfig();
-		
-		//1. 조회 : 호풀 할 떄 마다 객체를 생성
+
+		// 1. 조회 : 호풀 할 떄 마다 객체를 생성
 		MemberService memberService1 = appConfig.memberService();
-		
-		//2. 조회 : 호풀 할 떄 마다 객체를 생성
+
+		// 2. 조회 : 호풀 할 떄 마다 객체를 생성
 		MemberService memberService2 = appConfig.memberService();
-		
-		//참조값이 다른것을 확인
+
+		// 참조값이 다른것을 확인
 		System.out.println("memberService1 = " + memberService1);
 		System.out.println("memberService2 = " + memberService2);
-		
-		//memberService1 =/= memberService2
+
+		// memberService1 =/= memberService2
 		Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+
+	}
+
+	/* 이게 예시로 바깥에서 new 로 객체생성 불가능 !!!
+	 * public static void main(String[] args) {
+	 * 
+	 * SingletonService singletonService = new SingletonService(); SingletonService
+	 * singletonService2 = new SingletonService(); }
+	 */
+	
+	
+	@Test
+	@DisplayName("싱글톤 패턴을 적용한 객체 사용")
+	void singletionServiceTest() {
 		
+		SingletonService singletonService1 = SingletonService.getInstance();
+		SingletonService singletonService2 = SingletonService.getInstance();
 		
+		System.out.println("singletonService1 = " + singletonService1);
+		System.out.println("singletonService2 = " + singletonService2);
+		
+		Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+		// same ==
+		// equal 
 	}
 }
