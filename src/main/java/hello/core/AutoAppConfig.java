08a3c871b2858a0f289477fb998151d0ea71af1c
shinbeomsik <sbsdkssud@gmail.com>
2023-09-11 11:40:15 +0900

    탐색 위치와 기본 스캔 대상
    가장 최상위 폴더에 @ComponentScan붙혀서 사용하자  그러면 자동으로 하위 패키지들이 등록
    스프링부트 시작과 만들어지는(  **~**Application.java에 있는 @SpringBootApplication 에는  @ComponentScan이 있다)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,14 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION , classes = Configuration.class)
+		basePackages = "hello.core.member" , //탐색할 패키지의 시작 위치를 지정 --> 이 패키지를 시작으로 하위 패키지 모두 탐색 
+				   //= {"hello.core" , "hello.service"} 이런식으로 여러 시작위치를 지정할수도 있다.
+		basePackageClasses = AutoAppConfig.class,
+		excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION , classes = Configuration.class)
 		)
+//만약에 아무것도 지정하지 않으면 @ComponentScan 이 붙은 설정 정보의 클래시의 패키지가 시작위치가 된다.
+//관례는 프로젝트 루트(기본 가장 최상위폴더) 에 지정한다
+//스프링부트는 @SpringBootApplication(시작과 동시에 만들어지는 파일) 여기에도 @ComponentScan이 있긴하다 그래서 자동으로 등록
 public class AutoAppConfig {
 	
 }
