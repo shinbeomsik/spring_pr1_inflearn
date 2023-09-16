14e443bf8c7279e3323d774bd52e3558faaec568
shinbeomsik <sbsdkssud@gmail.com>
2023-09-16 22:13:12 +0900

    @Autowired필드명, @Quailifier,@Primary
    @Autowired 매칭같은경우
     - 타입매칭
     - 타입매칭의 결과가 2개 이상일때는 필드 명, 파라미터 명으로 빈 이름을 매칭
    
    @Quailifier :추가 구분자를 붙여주는 방식 ==> 주입시 추가적인 방법을 제공하는것이지만 빈 이름을 변경하는 것은 아니다.
     - @Quailifier끼리 매칭
     - 빈 이름  매칭
     - noSuchBeanDefinitionException 예외발생
    
    @Primary : 우선순위를 정하는 방법 @Autowired시에 여러빈이 매칭된다면 @Primary가 우선권을 가진다!
    
    @Primary 는 기본값처럼 동작하고 @Quailifier  는 매우 상세하게 동작
    스프링같은경우 자동 보다는 수동이 우선순위가 높다 그래서 @Quailifier 가 우선권이 높다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package hello.core.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import hello.core.discount.DiscountPolicy;
@@ -11,34 +12,31 @@ import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-	
+
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
-	
-	
-	/*  @RequiredArgsConstructor이게 있으면 자동으로 이 밑에 있는걸 만들어준다.!!!
-	 * @Autowired //이거 한곳에만 있으면 생략가능 public OrderServiceImpl(MemberRepository
-	 * memberRepository, DiscountPolicy discountPolicy) { this.memberRepository =
-	 * memberRepository; this.discountPolicy = discountPolicy; }
-	 */
- 
+
+	// @RequiredArgsConstructor이게 있으면 자동으로 이 밑에 있는걸 만들어준다.!!!
+	@Autowired // 이거 한곳에만 있으면 생략가능
+	public OrderServiceImpl(MemberRepository memberRepository,  DiscountPolicy discountPolicy) {
+		this.memberRepository = memberRepository;
+		this.discountPolicy = discountPolicy;
+	}
+
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
 		Member member = memberRepository.findById(memberId);
 		int discountPrice = discountPolicy.discount(member, itemPrice);
-		
+
 		return new Order(memberId, itemName, itemPrice, discountPrice);
 	}
-	
-		
-	//테스트 용도
+
+	// 테스트 용도
 	public MemberRepository getMemberRepository() {
 		return memberRepository;
 	}
-	
-	
 
 }
 
