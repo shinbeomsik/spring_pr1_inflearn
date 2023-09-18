832c991a3130f0a546a13edb41c04086410edc6b
shinbeomsik <sbsdkssud@gmail.com>
2023-09-18 17:54:11 +0900

    에노테이션 @PostConstruct , @PreDestory
    기본적으로 이 에노테이션을 사용하고
    외부 라이브러르는 적용하지 못하기 때문에 그때는 @Bean의 기능을 사용

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 //import org.springframework.beans.factory.DisposableBean;
 //import org.springframework.beans.factory.InitializingBean;
 
@@ -31,13 +34,14 @@ public class NetworkClient {
 		System.out.println("colse: " + url);
 	}
 	
-	
+	@PostConstruct
 	public void init() {
 		System.out.println("NetworkClient.init");
 		connect();
 		call("초기화 연결 메세지");
 	}
 
+	@PreDestroy
 	public void close()  {
 		System.out.println("NetworkClient.close");
 		disconnect();
