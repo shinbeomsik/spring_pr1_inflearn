a20be9919ebdd6dd3c2d2044f84e419f05b9443a
shinbeomsik <sbsdkssud@gmail.com>
2023-09-24 00:19:55 +0900

    스코프와 Provider
    ObjectProvicer 덕분에 ObjectProvider.getObject() 를 호출하는 시점까지request scope 빈의 생성을 지연가능

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,16 +15,18 @@ import lombok.RequiredArgsConstructor;
 public class LogDemoController {
 	
 	private final LogDemoService logDemoService;
-	private final MyLogger myLogger;
+	private final ObjectProvider<MyLogger>  myLoggerProvider;
 	
 	
 	@RequestMapping("log-demo")
 	@ResponseBody
-	public String logDemo(HttpServletRequest requset) {
+	public String logDemo(HttpServletRequest requset) throws InterruptedException {
+		MyLogger myLogger = myLoggerProvider.getObject();
 		String requestURL = requset.getRequestURI().toString();
 		myLogger.setRequestURL(requestURL);
 		
 		myLogger.log("controller test");
+		Thread.sleep(1000);
 		logDemoService.logic("testId");
 		return "OK";
 	}
